afe637ca52a4ab0e5f6f146d40db68b3b05f2028
Francisco Pacio <fpaciook@gmail.com>
2020-09-24 12:59:31 -0300

    Chapter 5: Perspective

--- perspective.py
+++ perspective.py
@@ -1,10 +1,17 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('imgs/algo.jpg')
+img = cv2.imread('imgs/naipe.jpg')
 
-img = cv2.resize(img, (480,640))
+width, height = 250,350
 
-cv2.imshow("Imagen", img)
+pts1 = np.float32([[129,150],[187,128],[244,196],[178,226]])
+pts2 = np.float32([[0,0],[width,0],[width,height],[0,height ]])
+
+matrix = cv2.getPerspectiveTransform(pts1,pts2)
+
+imgOutput = cv2.warpPerspective(img, matrix,(width,height))
+
+cv2.imshow("Perspective", imgOutput)
 
 cv2.waitKey(0)
